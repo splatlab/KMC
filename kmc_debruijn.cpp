15130357fb5abeade2316b56640526b28c663b60
Prashant Pandey <prashant.prashn@gmail.com>
2016-05-01 19:19:49 -0400

    Added updated de bruijn graph benchmark.

--- kmc_debruijn.cpp
+++ kmc_debruijn.cpp
@@ -40,6 +40,29 @@
 
 using namespace std;
 
+enum DNA_MAP {C, A, T, G};  // A=1, C=0, T=2, G=3
+
+/**
+ * Converts a string of "ATCG" to a uint64_t
+ * where each character is represented by using only two bits
+ */
+uint64_t str_to_int(string str)
+{
+	uint64_t strint = 0;
+	for (auto it = str.begin(); it != str.end(); it++) {
+		uint8_t curr = 0;
+		switch (*it) {
+			case 'A': { curr = DNA_MAP::A; break; }
+			case 'T': { curr = DNA_MAP::T; break; }
+			case 'C': { curr = DNA_MAP::C; break; }
+			case 'G': { curr = DNA_MAP::G; break; }
+		}
+		strint = strint | curr;
+		strint = strint << 2;
+	}
+	return strint >> 2;
+}
+
 /* Print elapsed time using the start and end timeval */
 void print_time_elapsed(string desc, struct timeval* start, struct timeval* end) 
 {
@@ -67,6 +90,7 @@ int main ( int argc, char *argv[] )
 	CKMCFile kmer_database_list;
 	CKMCFile kmer_database_rand;
 	vector<CKmerAPI> kmers;
+	vector<uint32_t> path_lengths;
 	CKmerAPI kmer(28);
 	uint32_t counter;
 	struct timeval start, end;
@@ -88,6 +112,14 @@ int main ( int argc, char *argv[] )
 	cout << "Total kmers: " << i << endl;
 	kmer_database_list.Close();
 
+	/* write the kmers to the disk */
+	ofstream ofs;
+	ofs.open("kmers_seed");
+	for (CKmerAPI kmer : kmers) {
+		ofs << str_to_int(kmer.to_string()) << "\n" ;
+	}
+	ofs.close();
+
 	if (!kmer_database_rand.OpenForRA(filename)) {
 		cout << "Can not open the database file" << endl;
 		return EXIT_FAILURE;
@@ -95,16 +127,17 @@ int main ( int argc, char *argv[] )
 
 	cout << "Calc the longest path in the de bruijn graph" << endl;
 	uint32_t max_size = 0;
+	gettimeofday(&start, &tzp);
 	for (CKmerAPI kmer : kmers) {
 		if (!kmer_database_rand.CheckKmer(kmer, counter)) {
 			cout << "Can not find the kmer: " << kmer.to_string() << endl;
 			continue;
 		}
 		else {
-			set<string> path;
+			set<uint64_t> path;
 			/* push the current node in the path list */
 			string str = kmer.to_string();
-			path.insert(str);
+			path.insert(str_to_int(str));
 			while (true) {
 				int next_kmerA = 0, next_kmerC = 0, next_kmerT = 0, next_kmerG = 0;
 				CKmerAPI kmerA(28), kmerC(28), kmerT(28), kmerG(28);
@@ -120,7 +153,7 @@ int main ( int argc, char *argv[] )
 				/* If found then push node in the path list */
 				if (kmer_database_rand.CheckKmer(kmerA, counter)) {
 					str = kmerA.to_string();
-					if (!path.insert(kmerA.to_string()).second) {
+					if (!path.insert(str_to_int(kmerA.to_string())).second) {
 						/*Found a loop*/
 						break;
 					}
@@ -128,7 +161,7 @@ int main ( int argc, char *argv[] )
 				}
 				if (kmer_database_rand.CheckKmer(kmerC, counter)) {
 					str = kmerC.to_string();
-					if (!path.insert(kmerC.to_string()).second) {
+					if (!path.insert(str_to_int(kmerC.to_string())).second) {
 						/*Found a loop*/
 						break;
 					}
@@ -136,7 +169,7 @@ int main ( int argc, char *argv[] )
 				}
 				if (kmer_database_rand.CheckKmer(kmerT, counter)) {
 					str = kmerT.to_string();
-					if (!path.insert(kmerT.to_string()).second) {
+					if (!path.insert(str_to_int(kmerT.to_string())).second) {
 						/*Found a loop*/
 						break;
 					}
@@ -144,7 +177,7 @@ int main ( int argc, char *argv[] )
 				}
 				if (kmer_database_rand.CheckKmer(kmerG, counter)) {
 					str = kmerG.to_string();
-					if (!path.insert(kmerG.to_string()).second) {
+					if (!path.insert(str_to_int(kmerG.to_string())).second) {
 						/*Found a loop*/
 						break;
 					}
@@ -159,12 +192,23 @@ int main ( int argc, char *argv[] )
 					break;
 				}
 			}
+			path_lengths.push_back(path.size());
 			if (path.size() > max_size) {
-				cout << "Path size: " << path.size() << endl;
 				max_size = path.size();
 			}
 		}
 	}
+	gettimeofday(&end, &tzp);
+	print_time_elapsed("", &start, &end);
+
+	cout << "Max path size: " << max_size << endl;
+
+	ofstream res;
+	res.open("debruijn_paths");
+	for (uint32_t len : path_lengths) {
+		res <<  len << "\n" ;
+	}
+	res.close();
 
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
